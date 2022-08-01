d8ce2ccd241ee07c088e137801edb050a01b657c
lsh <linsihang@zkhb.cn>
2022-08-01 17:58:40 +0800

    代理模式-下载视频

---
+++ src/struction_proxy_partner/code/media/MediaProxyClient.java
@@ -0,0 +1,36 @@
+package struction_proxy_partner.code.media;
+
+import struction_proxy_partner.code.media.download.YouTubeDownloader;
+import struction_proxy_partner.code.media.proxy.YouTubeCacheProxy;
+import struction_proxy_partner.code.media.some_cool_media_library.ThirdPartyYouTubeClass;
+
+public class MediaProxyClient {
+
+    public static void main(String[] args) {
+        YouTubeDownloader naiveDownloader = new YouTubeDownloader(new ThirdPartyYouTubeClass());
+        YouTubeDownloader smartDownloader = new YouTubeDownloader(new YouTubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+
+    }
+
+    private static long test(YouTubeDownloader downloader) {
+        long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+
+}
---
+++ src/struction_proxy_partner/code/media/some_cool_media_library/ThirdPartyYouTubeLib.java
@@ -0,0 +1,9 @@
+package struction_proxy_partner.code.media.some_cool_media_library;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYouTubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
